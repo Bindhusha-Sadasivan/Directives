030001575f49c2a543dcc639c4ee5512aaab9b35
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-08 07:00:34 +0530

    91 - ngFor and ngIf Recap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Directives';
+
+  numbers = [1,2,3,4,5,6];
+  oddNumbers:any = [];
+  evenNumbers:any = [];
+  showNumbers:boolean = false;
+
+  onToggle(){
+    this.showNumbers = !this.showNumbers;
+    this.numbers.forEach((element:any) => {
+      if(element%2 ===0){
+        this.evenNumbers.push(element);
+      }
+      else{
+        this.oddNumbers.push(element);
+      }
+    });
+  }
 }
